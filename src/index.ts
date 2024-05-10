3ad385e4bf7a1407cacd8c2213fcea4ec5940b36
Baltazar Cortina <baltazar.cortina@radiumrocket.com>
2024-05-10 02:28:59 -0300

    Fix logic for serverless deployment

--- src/index.ts
+++ src/index.ts
@@ -17,20 +17,22 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
+initFirebaseApp();
+
+if (process.env.IS_LOCAL !== 'true') {
+  app.use(async (_req, _res, next) => {
+    await connectDB();
+    next();
+  });
+}
+
 app.use('/', routes);
 
-if (process.env.IS_LOCAL) {
+if (process.env.IS_LOCAL === 'true') {
   app.listen(PORT, async () => {
     console.log(`Server listening on port ${PORT}`);
     await connectDB();
     console.log(`Database connected`);
-    initFirebaseApp();
-  });
-} else {
-  app.use(async (req, res, next) => {
-    await connectDB();
-    initFirebaseApp();
-    next();
   });
 }
 
